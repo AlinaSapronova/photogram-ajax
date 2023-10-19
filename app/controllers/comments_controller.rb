6d2983ec8ef3a1a79bff6d60316ac123cd58f795
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-10-19 15:47:20 +0000

    created destroy function with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -59,8 +59,12 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
+    
+    format.js do
+      render template: "comments/destroy"
     end
   end
+end
 
   private
     # Use callbacks to share common setup or constraints between actions.
